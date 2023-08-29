cb07926ebbefc2e308cdafaa89b3bce44e2013ec
Adrian Garic <adrian.garic007@gmail.com>
2023-08-29 15:50:44 +0200

    emoji stranica i calorie calculator

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,126 @@
-<template>
-   <div>
-    <div id="header-container">
-      <Header 
-        @scrollToContact="scrollToContact" 
-        @scrollToAbout="scrollToAbout" 
-        @scrollToCalorieCalculator="scrollToCalorieCalculator"
-        @dropdown="toggleTheDropdown" 
-        @clearCalorieInput="closeCalorieCalculator"
-      />
-    </div>
-    <div id="calculator-container" >
-      <CalorieCalculator 
-        @close="closeCalorieCalculator" 
-        v-show="showTheDropdown && showCalorieCalculator"
-        :resetForm="resetForm"
+  <template>
+    <div>
+      <div id="header-container">
+        <Header 
+          @scrollToContact="scrollToContact" 
+          @scrollToAbout="scrollToAbout" 
+          @scrollToCalorieCalculator="scrollToCalorieCalculator"
+          @scrollToEmojiPage="scrollToEmojiPage"
+          @dropdown="toggleTheDropdown" 
+          @clearCalorieInput="closeCalorieCalculator"
         />
+      </div>
+      <div id="calculator-container">
+        <CalorieCalculator 
+          ref="calorieCalculator"
+          @close="closeCalorieCalculator" 
+          v-show="showTheDropdown && showCalorieCalculator"
+          />
+      </div>
+      <router-view v-if="showCalorieCalculator" ></router-view>
+      <Motivation />
+      <Lifestyle />
+      <FAQ /> 
+      <Contact />
+      <Footer />
+      <div id="emoji-container">
+        <EmojiPage 
+          v-show="showTheEmojiPage"  
+        />
+      </div>
     </div>
-    <Motivation />
-    <Lifestyle />
-    <FAQ /> 
-    <Contact />
-    <router-view v-if="showCalorieCalculator"/>
-    <Footer />
-  </div>
-</template>
+  </template>
 
-<script>
-  import Header from "@/components/Header.vue";
-  import Motivation from "@/components/Motivation.vue";
-  import Lifestyle from "@/components/Lifestyle.vue";
-  import FAQ from "@/components/FAQ.vue";
-  import Contact from "@/components/Contact.vue";
-  import Footer from "@/components/Footer.vue";
-  import CalorieCalculator from "@/components/CalorieCalculator.vue";
+  <script>
+    import Header from "@/components/Header.vue";
+    import Motivation from "@/components/Motivation.vue";
+    import Lifestyle from "@/components/Lifestyle.vue";
+    import FAQ from "@/components/FAQ.vue";
+    import Contact from "@/components/Contact.vue";
+    import Footer from "@/components/Footer.vue";
+    import CalorieCalculator from "@/components/CalorieCalculator.vue"; 
+    import EmojiPage from "@/components/EmojiPage.vue"; 
 
-export default {
-  name: "App",
-  components: {
-    Header,
-    Motivation,
-    Lifestyle,
-    FAQ,
-    Contact,
-    Footer,
-    CalorieCalculator,
-  },
-  data() {
-    return {
-      showTheDropdown: false,
-      showCalorieCalculator: false,
-    };
-  },
-  methods: {
-    scrollToContact() {
-      const contactSection = document.getElementById('contact-form');
-      if (contactSection) {
-        contactSection.scrollIntoView({ behavior: 'smooth' });
-      }
+  export default {
+    name: "App",
+    components: {
+      Header,
+      Motivation,
+      Lifestyle,
+      FAQ,
+      Contact,
+      Footer,
+      CalorieCalculator,
+      EmojiPage,
+
     },
-    scrollToAbout() {
-      const aboutSection = document.getElementById('about-section');
-      if (aboutSection) {
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
-      }
+    data() {
+      return {
+        showTheDropdown: false,
+        showCalorieCalculator: false,
+        showTheEmojiPage: false,
+      };
     },
-    scrollToCalorieCalculator() {
-    const calorieCalculatorSection = document.getElementById('calculator-container');
-    if (calorieCalculatorSection) {
-      calorieCalculatorSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  },
-  toggleTheDropdown() {
-      this.showTheDropdown = !this.showTheDropdown;
-        if (this.showTheDropdown) {
-          this.showCalorieCalculator = true;
-        } else {
-          this.showCalorieCalculator = false;
-          window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page
+    methods: {
+      scrollToContact() {
+        const contactSection = document.getElementById('contact-form');
+        if (contactSection) {
+          contactSection.scrollIntoView({ behavior: 'smooth' });
         }
       },
-    closeCalorieCalculator() {
-      this.showCalorieCalculator = false; 
-      if (this.$route.name === "CalorieCalculator") {
-        this.$router.replace({ name: "home" });
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }
-      this.resetForm();
-    },
-    resetForm() {
-      if (this.$refs.calorieForm) {
-    this.$refs.calorieForm.$el.reset(); // Reset the form element directly
-  }
-
-      // Reset form fields manually
-      this.showCalorieCalculator = false;
-      this.showTheDropdown = false;
-      this.$nextTick(() => {
-        const form = document.getElementById('calorie-form');
-        if (form) {
-          form.reset();
+      scrollToAbout() {
+        const aboutSection = document.getElementById('about-section');
+        if (aboutSection) {
+          aboutSection.scrollIntoView({ behavior: 'smooth' });
         }
-      });
-      window.location.reload();
+      },
+      scrollToCalorieCalculator() {
+        const calorieCalculatorSection = document.getElementById('calculator-container');
+        if (calorieCalculatorSection) {
+          calorieCalculatorSection.scrollIntoView({ behavior: 'smooth' });
+      }
     },
+      scrollToEmojiPage(){
+          const emojiPageSection = document.getElementById('emoji-page');
+          if (emojiPageSection) {
+            this.showTheEmojiPage = true;
+            this.$nextTick(() => {
+            emojiPageSection.scrollIntoView({ behavior: 'smooth' });
+      });
+    }
+  },
+    toggleTheDropdown() {
+        this.showTheDropdown = !this.showTheDropdown;
+          if (this.showTheDropdown) {
+            this.showCalorieCalculator = true;
+          } else {
+            this.showCalorieCalculator = false;
+            window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page
+          }
+        },
+      closeCalorieCalculator() {
+
+        if (this.$route && this.$route.name === "CalorieCalculator") {
+          this.$router.replace({  name: "home"});
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }
+        this.showCalorieCalculator = false;
+        this.$refs.calorieCalculator.resetForm();
+      },
   },
 };
-</script>
+  </script>
 
-<style>
-  *{
-    margin: 0;
-    padding: 0;
-    font-family: 'Poppins', sans-serif;
-  }
-  #calculator-container{
-    padding-top: 65px;
-  }
-
-</style>
+  <style>
+    *{
+      margin: 0;
+      padding: 0;
+      font-family: 'Poppins', sans-serif;
+    }
+    #calculator-container{
+      padding-top: 65px;
+    }
+  </style>
 
 
 
