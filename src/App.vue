a2af5607bf6394b7cfe3d979c1c0358c57358e91
Adrian Garic <adrian.garic007@gmail.com>
2023-09-15 05:31:50 +0200

    Promjene

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
   <template>
-    <div>
       <div id="header-container">
         <Header 
           @scrollToContact="scrollToContact" 
           @scrollToAbout="scrollToAbout" 
           @scrollToCalorieCalculator="scrollToCalorieCalculator"
           @scrollToEmojiPage="scrollToEmojiPage"
+          @scrollToWaterTracker="scrollToWaterTracker"
           @dropdown="toggleTheDropdown" 
           @clearCalorieInput="closeCalorieCalculator"
+          @closeWaterTracker="closeWaterTracker"
         />
       </div>
       <div id="calculator-container">
@@ -17,18 +18,44 @@
           v-show="showTheDropdown && showCalorieCalculator"
           />
       </div>
-      <router-view v-if="showCalorieCalculator" ></router-view>
+      <div id="water-container" >
+        <WaterTracker 
+          v-show="showWaterTracker"
+          @closeWater="closeWaterTracker"
+        />
+      </div>
       <Motivation />
       <Lifestyle />
-      <FAQ /> 
-      <Contact />
+      <FAQ />
+      <Contact
+        @updateShowContactForm="updateShowContactForm" 
+      />
+      <div id="cf-container">
+        <ContactForm 
+          v-show="showContactForm"
+        />
+      </div>
       <Footer />
       <div id="emoji-container">
         <EmojiPage 
-          v-show="showTheEmojiPage"  
+          v-show="showTheEmojiPage" 
+          @scrollToSad="scrollToSad"  
+          @scrollToNeutral="scrollToNeutral"
+          @scrollToHappy="scrollToHappy"
         />
       </div>
-    </div>
+      <div id="sad-container" v-show="showSad" >
+        <Sad ref="sadComponent"
+        />
+      </div>
+      <div id="neutral-container" v-show="showNeutral" >
+        <Neutral ref="neutralComponent"
+        />
+      </div>
+      <div id="happy-container" v-show="showHappy" >
+        <Happy ref="happyComponent"
+        />
+      </div>
   </template>
 
   <script>
@@ -40,6 +67,11 @@
     import Footer from "@/components/Footer.vue";
     import CalorieCalculator from "@/components/CalorieCalculator.vue"; 
     import EmojiPage from "@/components/EmojiPage.vue"; 
+    import Sad from "@/components/Sad.vue"; 
+    import Neutral from "@/components/Neutral.vue"; 
+    import Happy from "@/components/Happy.vue"; 
+    import WaterTracker from "@/components/WaterTracker.vue"; 
+    import ContactForm from "@/components/ContactForm.vue"; 
 
   export default {
     name: "App",
@@ -52,63 +84,135 @@
       Footer,
       CalorieCalculator,
       EmojiPage,
-
+      Sad,
+      Neutral,
+      Happy,
+      WaterTracker,
+      ContactForm
     },
+
     data() {
       return {
         showTheDropdown: false,
         showCalorieCalculator: false,
         showTheEmojiPage: false,
+        showSad: false,
+        showNeutral: false,
+        showHappy: false,
+        showWaterTracker: false, 
+        showContactForm: false, 
       };
     },
     methods: {
       scrollToContact() {
-        const contactSection = document.getElementById('contact-form');
-        if (contactSection) {
-          contactSection.scrollIntoView({ behavior: 'smooth' });
-        }
-      },
+        const contactSection = document.getElementById('contact');
+          if (contactSection) {
+            contactSection.scrollIntoView({ behavior: 'smooth' });
+          }
+        },
       scrollToAbout() {
         const aboutSection = document.getElementById('about-section');
-        if (aboutSection) {
-          aboutSection.scrollIntoView({ behavior: 'smooth' });
-        }
-      },
+          if (aboutSection) {
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+          }
+        },
       scrollToCalorieCalculator() {
         const calorieCalculatorSection = document.getElementById('calculator-container');
-        if (calorieCalculatorSection) {
-          calorieCalculatorSection.scrollIntoView({ behavior: 'smooth' });
-      }
-    },
+          if (calorieCalculatorSection) {
+            calorieCalculatorSection.scrollIntoView({ behavior: 'smooth' });
+          }
+        },
+      scrollToWaterTracker() {
+        const waterTrackerSection = document.getElementById('water-container');
+          if (waterTrackerSection) {
+            this.showWaterTracker = true;
+            this.showCalorieCalculator = false;
+            this.$nextTick(() => {
+            waterTrackerSection.scrollIntoView({ behavior: 'smooth' });
+            });
+          }
+        },
       scrollToEmojiPage(){
-          const emojiPageSection = document.getElementById('emoji-page');
+        const emojiPageSection = document.getElementById('emoji-page');
           if (emojiPageSection) {
             this.showTheEmojiPage = true;
             this.$nextTick(() => {
-            emojiPageSection.scrollIntoView({ behavior: 'smooth' });
-      });
-    }
-  },
-    toggleTheDropdown() {
+              emojiPageSection.scrollIntoView({ behavior: 'smooth' });
+            });
+          }
+        },
+      scrollToSad(){
+        const sadSection = this.$refs.sadComponent.$el;
+          if (sadSection) {
+            this.showHappy = false;
+            this.showNeutral = false;
+            this.showSad = true;
+            this.$nextTick(() => {
+              sadSection.scrollIntoView({ behavior: 'smooth' });
+            });
+          }
+        },
+      scrollToNeutral(){
+        const neutralSection = this.$refs.neutralComponent.$el;
+          if (neutralSection) {
+            this.showHappy = false;
+            this.showSad = false;
+            this.showNeutral = true;
+            this.$nextTick(() => {
+              neutralSection.scrollIntoView({ behavior: 'smooth' });
+            });
+          }
+        },
+      scrollToHappy(){
+        const happySection = this.$refs.happyComponent.$el;
+          if (happySection) {
+            this.showSad = false;
+            this.showNeutral = false;
+            this.showHappy = true;
+            this.$nextTick(() => {
+              happySection.scrollIntoView({ behavior: 'smooth' });
+            });
+          }
+        },
+          
+      toggleTheDropdown() {
         this.showTheDropdown = !this.showTheDropdown;
           if (this.showTheDropdown) {
             this.showCalorieCalculator = true;
+            this.showWaterTracker = false;
           } else {
             this.showCalorieCalculator = false;
             window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page
           }
         },
-      closeCalorieCalculator() {
+        updateShowContactForm() {
+          const contactFormSection = document.getElementById('cf-container');
+          if (contactFormSection) {
+            this.showContactForm = true;
+            this.$nextTick(() => {
+              contactFormSection.scrollIntoView({ behavior: 'smooth'  });
+            });
+          }
+        },
+        closeCalorieCalculator() {
 
-        if (this.$route && this.$route.name === "CalorieCalculator") {
+          if (this.$route && this.$route.name === "CalorieCalculator") {
+            this.$router.replace({  name: "home"});
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }
+          this.showCalorieCalculator = false;
+          this.$refs.calorieCalculator.resetForm();
+        },
+      closeWaterTracker() {
+
+        if (this.$route && this.$route.name === "WaterTracker") {
           this.$router.replace({  name: "home"});
           window.scrollTo({ top: 0, behavior: "smooth" });
         }
-        this.showCalorieCalculator = false;
-        this.$refs.calorieCalculator.resetForm();
+        this.showWaterTracker = false;
       },
-  },
-};
+    },
+  };
   </script>
 
   <style>
@@ -118,6 +222,9 @@
       font-family: 'Poppins', sans-serif;
     }
     #calculator-container{
+      padding-top: 65px;
+    }
+    #water-container{
       padding-top: 65px;
     }
   </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import CalorieCalculator from '@/components/CalorieCalculator.vue';
+import EmojiPage from '@/components/EmojiPage.vue'; 
+import Sad from '@/components/Sad.vue'; 
+import Neutral from '@/components/Neutral.vue'; 
+import Happy from '@/components/Happy.vue'; 
+import WaterTracker from '@/components/WaterTracker.vue'; 
+import ContactForm from '@/components/ContactForm.vue'; 
 
 const routes = [
   {
@@ -11,13 +17,44 @@ const routes = [
   {
     path: '/CalorieCalculator',
     name: 'CalorieCalculator',
-    component: CalorieCalculator,
-  }
-]
+    component: CalorieCalculator
+  },
+  {
+    path: '/EmojiPage',
+    name: 'EmojiPage',
+    component: EmojiPage,
+  },
+  {
+    path: '/Sad',
+    name: 'Sad',
+    component: Sad
+  },
+  {
+    path: '/Neutral',
+    name: 'Neutral',
+    component: Neutral
+  },
+  {
+    path: '/Happy',
+    name: 'Happy',
+    component: Happy
+  },
+  {
+    path: '/WaterTracker',
+    name: 'WaterTracker',
+    component: WaterTracker
+  },
+  {
+    path: '/ContactForm',
+    name: 'ContactForm',
+    component: ContactForm
+  },
+  
+];
 
-const router = createRouter({
+const router = createRouter({ 
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
 export default router;
